782e2e7257df8766a141b4b1fac8081a935ed913
JohnGuilding <jwguilding@gmail.com>
2022-09-14 22:48:16 +0100

    Test lint-staged

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -231,7 +231,7 @@ contract Pool is SuperAppBase, IAqueductHost {
     }
 
     function getUserCumulativeDelta(
-    address token,
+        address token,
     address user,
     uint256 timestamp
     ) public view returns (uint256 cumulativeDelta) {
